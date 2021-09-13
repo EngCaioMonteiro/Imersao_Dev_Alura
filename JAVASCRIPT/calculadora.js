f4ebdf206d840210960ea080a6a8d862f1c3f609
Caio Oliveira Monteiro <caio.monteiro@grupoicts.com.br>
2021-09-13 14:07:31 -0400

    Calculadora da Semana de Imersão Dev da Alura

---
+++ JAVASCRIPT/calculadora.js
@@ -0,0 +1,25 @@
+var nome = "Caio Monteiro";
+var notaDoPrimeiroBimestre = 9.6;
+var notaDoSegundoBimestre = 7;
+var notaDoTerceiroBimestre = 4;
+var notaDoQuartoBimestre = 2;
+var notaFinal =
+  (notaDoPrimeiroBimestre +
+    notaDoSegundoBimestre +
+    notaDoTerceiroBimestre +
+    notaDoQuartoBimestre) /
+  4;
+var notaFixada = notaFinal.toFixed(1);
+
+console.log("Bem vindo " + nome);
+console.log(notaDoPrimeiroBimestre);
+console.log(notaFinal);
+console.log(notaFixada);
+
+/*  Variáveis (var)
+    strings
+    console.log
+    toFixed(Number)
+    Operações Matemáticas
+    Concatenação
+    */
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Semana de imersão Dev da Alura
